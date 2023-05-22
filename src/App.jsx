28f03c0f7048cf1ef35e1e27e49b5a7203db2930
ChristianParodi <75328764+ChristianParodi@users.noreply.github.com>
2023-05-22 14:25:39 +0200

    Setuppato il progetto

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { 
+  Container, 
+  Heading,
+  AbsoluteCenter,
+  Image, 
+  Button,
+  Flex,
+  Spacer,
+  Box,
+  Icon,
+  Text,
+  Center
+} from "@chakra-ui/react"
+import { MdSchool } from 'react-icons/md'
+import { FaUserTie } from 'react-icons/fa'
+import viteLogo from "/vite.svg"
+import React from "react"
+import { motion } from 'framer-motion'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container centerContent minH={"100vh"}>
+        <Flex
+          flexDir={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          gap={5}
+          ml={"300px"}
+        >
+          <Image src={viteLogo} boxSize="200px" />
+          <Heading>
+            Benvenuto su (titolo)
+          </Heading>
+          <Heading size={'md'}>
+            Blurb Blurb Blurb Blurb Blurb
+          </Heading>
+          <Flex gap={100}>
+            <Button
+              w="fit-content"
+              h={"fit-content"}
+              display={"flex"}
+              flexDir={"column"}
+              py={5}
+              >
+              <Icon as={MdSchool} boxSize={"80px"} />
+              <Text>
+                Studente
+              </Text>
+            </Button>
+            <Button
+             w="fit-content"
+             h={"fit-content"}
+             display={"flex"}
+             flexDir={"column"}
+             py={5}
+             
+              >
+              <Icon as={FaUserTie} boxSize={"80px"} mb={2}/>
+              <Text>
+                Recruiter
+              </Text>
+            </Button>
+            <motion.div
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 1.1 }}
+            drag="x"
+            dragConstraints={{ left: -100, right: 100 }}
+            >
+
+            </motion.div>
+          </Flex>
+        </Flex>
+    </Container>
   )
 }
 
