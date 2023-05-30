f9a18cbf167184c8f9267b6e3d545276beb88b51
ChristianParodi <75328764+ChristianParodi@users.noreply.github.com>
2023-05-30 09:46:52 +0200

    Rancato via tutto

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,35 @@
-import { Heading, Flex, Spacer, Box, Icon, Text } from "@chakra-ui/react";
-
-import { MdSchool, MdSupportAgent } from "react-icons/md";
-
-import React from "react";
-import CustomButton from "./components/CustomButton";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Flex
-      bgColor={"primary.100"}
-      alignItems={"center"}
-      flexDir="column"
-      minH={"100vh"}
-      minW={"100vw"}
-    >
-      <Heading as="h1" size={"4xl"} mt={10}>
-        WorkPholio
-      </Heading>
-      <Heading as="h2" size={"sm"} color={"gray.500"} mt={5}>
-        Lorem ipsum dolor sit amet consectetur adipisicing
-        <br />
-        elit. Et ratione eligendi, beatae nam cupiditate error.
-      </Heading>
-      <Flex alignItems={"center"} gap={"15em"} mt={20}>
-        {/* Studente */}
-        <Box
-          boxShadow={"lg"}
-          minW={"300px"}
-          minH={"300px"}
-          bgColor={"blue.800"}
-          display={"flex"}
-          flexDir={"column"}
-          alignItems={"center"}
-        >
-          <Icon as={MdSchool} boxSize={"80%"} color={"white"} />
-          <Spacer />
-          <Flex
-            alignItems={"center"}
-            justifyContent={"center"}
-            bgColor={"white"}
-            w={"100%"}
-            h={"3.5em"}
-          >
-            <CustomButton colorScheme="yellow">Registrati</CustomButton>
-          </Flex>
-        </Box>
-        {/* Recruiter */}
-        <Box
-          boxShadow={"lg"}
-          minW={"300px"}
-          minH={"300px"}
-          bgColor={"blue.800"}
-          display={"flex"}
-          flexDir={"column"}
-          alignItems={"center"}
-        >
-          <Icon as={MdSupportAgent} boxSize={"80%"} color={"white"} />
-          <Spacer />
-          <Flex
-            alignItems={"center"}
-            justifyContent={"center"}
-            bgColor={"white"}
-            w={"100%"}
-            h={"3.5em"}
-          >
-            <CustomButton colorScheme="yellow">Registrati</CustomButton>
-          </Flex>
-        </Box>
-      </Flex>
-      <Text mt={20} fontSize={"lg"}>
-        Oppure se hai gia' un'account
-      </Text>
-      <CustomButton mt={5} colorScheme="yellow">
-        Accedi
-      </CustomButton>
-    </Flex>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./useTheme";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
