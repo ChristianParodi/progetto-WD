d57300d3a85ccfd4fc8b74b89179fc2b40a4b749
ChristianParodi <75328764+ChristianParodi@users.noreply.github.com>
2023-06-14 10:51:11 +0200

    Continuato il template

--- src/components/templates/DeveloperTemplate.jsx
+++ src/components/templates/DeveloperTemplate.jsx
@@ -1,9 +1,12 @@
+import { createRoot } from "react-dom/client";
+
 import {
   Button,
   Dialog,
   DialogBody,
   Typography,
 } from "@material-tailwind/react";
+
 import React, { forwardRef, useRef, useState } from "react";
 
 import {
@@ -12,6 +15,9 @@ import {
   CodeBracketIcon,
   LinkIcon,
 } from "@heroicons/react/24/outline";
+
+import { Editor } from "@tinymce/tinymce-react";
+
 import { VscTextSize, VscFile } from "react-icons/vsc";
 
 const AddMediaButton = forwardRef(
@@ -36,30 +42,71 @@ const AddMediaButton = forwardRef(
   }
 );
 
-const ChoiceDialog = ({ clickedBlock, open, handler }) => {
+const ChoiceDialog = ({
+  clickedBlock,
+  blocks,
+  open,
+  handler,
+  setClickedOperation,
+}) => {
   return (
-    <Dialog open={open} handler={handler}>
+    <Dialog
+      size="lg"
+      className="max-h-[90vh] overflow-auto"
+      open={open}
+      handler={handler}
+    >
       <DialogBody className="w-full h-full">
-        <div className="flex items-center justify-center gap-4 mb-6">
-          <div className="w-32 flex flex-col items-center hover:bg-gray-400 duration-200 cursor-pointer rounded-lg p-3">
+        <div className="flex items-center justify-center gap-4 mb-6 flex-wrap">
+          <div
+            onClick={() => {
+              setClickedOperation("image");
+              handler();
+            }}
+            className="w-32 flex flex-col items-center hover:bg-gray-400 duration-200 cursor-pointer rounded-lg p-3"
+          >
             <PhotoIcon className="w-24" />
             <Typography>Immagine</Typography>
           </div>
-          <div className="w-32 flex flex-col items-center hover:bg-gray-400 duration-200 cursor-pointer rounded-lg p-3">
+          <div
+            onClick={() => {
+              setClickedOperation("text");
+              handler();
+            }}
+            className="w-32 flex flex-col items-center hover:bg-gray-400 duration-200 cursor-pointer rounded-lg p-3"
+          >
             <VscTextSize className="text-[6rem] w-32" />
             <Typography>Testo</Typography>
           </div>
-          <div className="w-32 flex flex-col items-center hover:bg-gray-400 duration-200 cursor-pointer rounded-lg p-3">
+          <div
+            onClick={() => {
+              setClickedOperation("code");
+              handler();
+            }}
+            className="w-32 flex flex-col items-center hover:bg-gray-400 duration-200 cursor-pointer rounded-lg p-3"
+          >
             <CodeBracketIcon className="w-24" />
             <Typography>Codice</Typography>
           </div>
         </div>
-        <div className="flex items-center justify-center gap-4">
+        <div
+          onClick={() => {
+            setClickedOperation("link");
+            handler();
+          }}
+          className="flex items-center justify-center gap-4 flex-wrap"
+        >
           <div className="flex flex-col items-center hover:bg-gray-400 duration-200 cursor-pointer rounded-lg p-3">
             <LinkIcon className="w-24" />
             <Typography>Link</Typography>
           </div>
-          <div className="w-32 flex flex-col items-center hover:bg-gray-400 duration-200 cursor-pointer rounded-lg p-3">
+          <div
+            onClick={() => {
+              setClickedOperation("file");
+              handler();
+            }}
+            className="w-32 flex flex-col items-center hover:bg-gray-400 duration-200 cursor-pointer rounded-lg p-3"
+          >
             <VscFile className="text-[6rem] w-24" />
             <Typography>File</Typography>
           </div>
@@ -69,78 +116,184 @@ const ChoiceDialog = ({ clickedBlock, open, handler }) => {
   );
 };
 
+const TinyEditor = forwardRef((props, ref) => {
+  const tinyMceApiKey = import.meta.env.VITE_TINYMCE_API_KEY;
+
+  return (
+    <>
+      <Editor
+        apiKey={tinyMceApiKey}
+        onInit={(evt, editor) => (ref.current = editor)}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          width: "80%",
+          menubar: false,
+          plugins: [
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "code",
+            "help",
+            "wordcount",
+          ],
+          toolbar:
+            "undo redo | blocks | " +
+            "bold italic forecolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          content_style: `
+            body { 
+              font-family:Helvetica, Arial, sans-serif; 
+              font-size: 14px;
+              width: 80%; 
+            } `,
+        }}
+      />
+    </>
+  );
+});
+
 function DeveloperTemplate({ project }) {
-  const [clickedBlock, setClickedBlock] = useState(null);
+  const [clickedBlock, setClickedBlock] = useState(-1);
+  const [clickedOperation, setClickedOperation] = useState("");
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen((prev) => !prev);
+
+  const editorRef = useRef(null);
 
   const blocks = [];
   for (let i = 0; i < 6; i++) blocks.push(useRef(null));
 
   return (
-    <div className="h-[100vh] flex justify-center">
+    <div className="min-h-[100vh] flex justify-center">
       <div className="w-[80%] border-2 flex flex-col items-center py-5 gap-4">
+        {clickedBlock === 0 && clickedOperation === "text" && (
+          <>
+            <TinyEditor ref={editorRef} />
+            <Button
+              onClick={() => {
+                setClickedOperation("");
+                const node = document.createElement("DIV");
+                node.setHTML(editorRef.current.getContent());
+                blocks[0].current.innerHTML = "";
+                blocks[0].current.appendChild(node);
+              }}
+            >
+              Salva
+            </Button>
+          </>
+        )}
         <AddMediaButton
-          key={blocks[0]}
+          ref={blocks[0]}
           onClick={() => {
             setClickedBlock(0);
             handleOpen();
           }}
+          className={"normal-case"}
         />
-        <div className="w-[80%] flex gap-4">
+        <div className="w-full flex flex-col md:flex-row md:w-[80%] items-center gap-4">
           <ChoiceDialog
             clickedBlock={clickedBlock}
+            blocks={blocks}
             open={open}
             handler={handleOpen}
+            setClickedOperation={setClickedOperation}
           />
-          <AddMediaButton
-            ref={blocks[1]}
-            className="h-96 flex items-center"
-            iconProps="w-16"
-            onClick={() => {
-              setClickedBlock(1);
-              handleOpen();
-            }}
-          />
-          <AddMediaButton
-            ref={blocks[2]}
-            className="h-96 flex items-center"
-            iconProps="w-16"
-            onClick={() => {
-              setClickedBlock(2);
-              handleOpen();
-            }}
-          />
+          {clickedBlock === 1 && clickedOperation === "text" ? (
+            <>
+              <TinyEditor ref={editorRef} />
+              <Button onClick={() => setClickedOperation("")}>Salva</Button>
+            </>
+          ) : (
+            <AddMediaButton
+              ref={blocks[1]}
+              className="h-96 flex items-center"
+              iconProps="w-16"
+              onClick={() => {
+                setClickedBlock(1);
+                handleOpen();
+              }}
+            />
+          )}
+          {clickedBlock === 2 && clickedOperation === "text" ? (
+            <>
+              <TinyEditor ref={editorRef} />
+              <Button onClick={() => setClickedOperation("")}>Salva</Button>
+            </>
+          ) : (
+            <AddMediaButton
+              ref={blocks[2]}
+              className="h-96 flex items-center"
+              iconProps="w-16"
+              onClick={() => {
+                setClickedBlock(2);
+                handleOpen();
+              }}
+            />
+          )}
         </div>
-        <div className="w-[80%] flex gap-4">
-          <AddMediaButton
-            ref={blocks[3]}
-            className="h-96 flex items-center"
-            iconProps="w-16"
-            onClick={() => {
-              setClickedBlock(3);
-              handleOpen();
-            }}
-          />
-          <AddMediaButton
-            ref={blocks[4]}
-            className="h-96 flex items-center"
-            iconProps="w-16"
-            onClick={() => {
-              setClickedBlock(4);
-              handleOpen();
-            }}
-          />
+        <div className="w-full flex flex-col md:flex-row md:w-[80%] items-center gap-4">
+          {clickedBlock === 3 && clickedOperation === "text" ? (
+            <>
+              <TinyEditor ref={editorRef} />
+              <Button onClick={() => setClickedOperation("")}>Salva</Button>
+            </>
+          ) : (
+            <AddMediaButton
+              ref={blocks[3]}
+              className="h-96 flex items-center"
+              iconProps="w-16"
+              onClick={() => {
+                setClickedBlock(3);
+                handleOpen();
+              }}
+            />
+          )}
+          {clickedBlock === 4 && clickedOperation === "text" ? (
+            <>
+              <TinyEditor ref={editorRef} />
+              <Button onClick={() => setClickedOperation("")}>Salva</Button>
+            </>
+          ) : (
+            <AddMediaButton
+              ref={blocks[4]}
+              className="h-96 flex items-center"
+              iconProps="w-16"
+              onClick={() => {
+                setClickedBlock(4);
+                handleOpen();
+              }}
+            />
+          )}
         </div>
-        <AddMediaButton
-          ref={blocks[5]}
-          className="h-96 flex items-center"
-          iconProps="w-16"
-          onClick={() => {
-            setClickedBlock(5);
-            handleOpen();
-          }}
-        />
+        {clickedBlock === 5 && clickedOperation === "text" ? (
+          <>
+            <TinyEditor ref={editorRef} />
+            <Button onClick={() => setClickedOperation("")}>Salva</Button>
+          </>
+        ) : (
+          <AddMediaButton
+            ref={blocks[5]}
+            className="h-96 flex items-center"
+            iconProps="w-16"
+            onClick={() => {
+              setClickedBlock(5);
+              handleOpen();
+            }}
+          />
+        )}
       </div>
     </div>
   );
